88c822fa54fd6afb4e995ee74bb9c98938ab5483
Felix-fale <falefelix23@gmail.com>
2023-04-06 21:59:19 +0000

    feat: add other cors Link

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000/", "https://mern-task-app.vercel.app/"],
+    origin: ["http://localhost:3000", "https://mern-task-app.vercel.app"],
   })
 );
 app.use("/api/tasks", taskRoutes);
